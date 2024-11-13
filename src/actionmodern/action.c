a02fdf2bf66f820b313bc9f092bd43ac47cd4737
LittleCube <littlecubehax@gmail.com>
2024-11-13 14:34:18 -0500

    implement passing floats to actionTrace

--- src/actionmodern/action.c
+++ src/actionmodern/action.c
@@ -4,3 +4,21 @@ void actionTrace(const char* str)
 {
 	printf("%s\n", str);
 }
+
+void actionTrace(var val)
+{
+	switch (val.type)
+	{
+		case ACTION_STACK_VALUE_STRING:
+		{
+			printf("%s\n", val.value);
+			break;
+		}
+		
+		case ACTION_STACK_VALUE_F32:
+		{
+			printf("%.15g\n", val.value);
+			break;
+		}
+	}
+}
