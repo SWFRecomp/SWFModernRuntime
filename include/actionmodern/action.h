60ea660db29448d604a73e215846a220b105c58c
LittleCube <littlecubehax@gmail.com>
2024-11-13 14:40:29 -0500

    fix variable actionTrace

--- include/actionmodern/action.h
+++ include/actionmodern/action.h
@@ -3,3 +3,4 @@
 #include <stackvalue.h>
 
 void actionTrace(const char* str);
+void actionTraceVar(var val);
--- src/actionmodern/action.c
+++ src/actionmodern/action.c
@@ -5,19 +5,19 @@ void actionTrace(const char* str)
 	printf("%s\n", str);
 }
 
-void actionTrace(var val)
+void actionTraceVar(var val)
 {
 	switch (val.type)
 	{
 		case ACTION_STACK_VALUE_STRING:
 		{
-			printf("%s\n", val.value);
+			printf("%s\n", (char*) val.value);
 			break;
 		}
 		
 		case ACTION_STACK_VALUE_F32:
 		{
-			printf("%.15g\n", val.value);
+			printf("%.15g\n", (float) val.value);
 			break;
 		}
 	}
