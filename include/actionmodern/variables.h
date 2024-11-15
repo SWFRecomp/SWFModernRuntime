f4372d2321ed6cf58ad9a84285ac5d4c20f1b234
LittleCube <littlecubehax@gmail.com>
2024-11-14 19:38:44 -0500

    fix getVariable

--- include/actionmodern/variables.h
+++ include/actionmodern/variables.h
@@ -2,4 +2,13 @@
 
 #include <stackvalue.h>
 
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
 var* getVariable(u64 var_name);
+
+#ifdef __cplusplus
+}
+#endif
--- src/actionmodern/variables.cpp
+++ src/actionmodern/variables.cpp
@@ -4,7 +4,7 @@
 
 ankerl::unordered_dense::map<char*, var*> var_map;
 
-extern "C" var* getVariable(u64 var_name)
+var* getVariable(u64 var_name)
 {
 	char* var_str = (char*) var_name;
 	if (var_map.count(var_str) == 0)
