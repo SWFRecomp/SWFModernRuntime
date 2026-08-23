731ca0d49d7677d188c9b202b54d549e8cbc8b57
LittleCube <littlecubehax@gmail.com>
2026-08-23 15:45:05 -0400

    fix string memory leak

--- src/actionmodern/action.c
+++ src/actionmodern/action.c
@@ -479,6 +479,11 @@ void releaseObjectVar(SWFAppContext* app_context, ActionVar* var)
 			releaseObject(app_context, var->object);
 		});
 	}
+	
+	else if (IS_STR_T(var->type) && var->owns_memory)
+	{
+		FREE(var->str);
+	}
 }
 
 void copyReg(SWFAppContext* app_context)
